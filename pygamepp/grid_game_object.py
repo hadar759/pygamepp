5e649853862f706d6234d6793fd51b7afcb1d2cb
Hadar Dagan <hadarda11@gmail.com>
2019-11-25 15:53:39 +0200

    Self.block_size helps it work with game

--- pygamepp/grid_game_object.py
+++ pygamepp/grid_game_object.py
@@ -4,12 +4,13 @@ import pygame
 
 
 class GridGameObject:
-    def __init__(self, sprite: pygame.sprite, position: List):
+    def __init__(self, sprite: pygame.sprite, position: List, block_size):
         self.sprite = sprite
         colorkey = self.sprite.get_at((0, 0))
         self.sprite.set_colorkey(colorkey)
 
         self.position = position
+        self.block_size = block_size
 
     def display_object(self, screen: pygame.Surface, block_size: int):
-        screen.blit(self.sprite, (self.position[0] * block_size, self.position[1] * block_size))
+        screen.blit(self.sprite, (self.position[0] * self.block_size, self.position[1] * self.block_size))

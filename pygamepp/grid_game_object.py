bdb39ae01aebdb3200de8ab1d07d5758cc19eecb
Hadar Dagan <hadarda11@gmail.com>
2019-11-25 13:55:20 +0200

    Now able to use game object with grid games

---
+++ pygamepp/grid_game_object.py
@@ -0,0 +1,16 @@
+from typing import List
+
+import pygame
+
+
+class GridGameObject:
+    class GameObject:
+        def __init__(self, sprite: pygame.sprite, position: List):
+            self.sprite = sprite
+            colorkey = self.sprite.get_at((0, 0))
+            self.sprite.set_colorkey(colorkey)
+
+            self.position = position
+
+        def display_object(self, screen: pygame.Surface, block_size: int):
+            screen.blit(self.sprite, (self.position[0] * block_size, self.position[1] * block_size))

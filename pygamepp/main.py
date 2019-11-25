8969f627ae33530bc0d2e69c0215739605c659c3
Hadar Dagan <hadarda11@gmail.com>
2019-11-25 12:41:41 +0200

    Main now gets screen size

--- pygamepp/main.py
+++ pygamepp/main.py
@@ -3,12 +3,18 @@
 # Version 1.0
 # November 17th, 2019
 
+import ctypes
+
 from pygamepp import game
+
+USER32 = ctypes.windll.user32
 
 
 def main():
     """Call the game and run it"""
-    crazy_plane_game = game.Game()
+    width = USER32.GetSystemMetrics(0)
+    height = USER32.GetSystemMetrics(1)
+    crazy_plane_game = game.Game(width, height, 75)
     crazy_plane_game.run()
 
 

5956d971221c4eadb978b4433a5fe47a6c17ede7
Hadar Dagan <hadarda11@gmail.com>
2020-07-22 21:25:10 +0300

    Added everything needed to upload to pypi

--- setup.py
+++ setup.py
@@ -6,5 +6,7 @@ setuptools.setup(
     author="Hadar Dagan",
     description="This is a wrapper i made for pygame for my schoool projects",
     author_email="hadarda11@gmail.com",
-    packages=setuptools.find_packages()
+    packages=setuptools.find_packages(),
+	download_url="https://github.com/hadar759/pygamepp/archive/v_01.zip",
+	install_requires=["pygame"]
 )

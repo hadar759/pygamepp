72bffeb25a259e9372099f9a76502ae6d9d80698
Hadar Dagan <hadarda11@gmail.com>
2020-07-22 21:34:56 +0300

    Changed license

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="pygamepp",
-    version="0.1",
+    version="0.11",
     author="Hadar Dagan",
     description="This is a wrapper i made for pygame for my schoool projects",
     author_email="hadarda11@gmail.com",
     packages=setuptools.find_packages(),
-	download_url="https://github.com/hadar759/pygamepp/archive/v_01.zip",
+	download_url="https://github.com/hadar759/pygamepp/archive/v_011.tar.gz",
 	install_requires=["pygame"]
 )
